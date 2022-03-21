dedde072e0e6834e41f619f0ff615dfc2a6e9ec4
Clement <xiaobaiwhu@gmail.com>
2022-03-21 15:21:58 +0800

    feat:完善注册接口

--- app/src/main/java/com/yuebing/aicoursesystemandroid/task/RegisterTask.java
+++ app/src/main/java/com/yuebing/aicoursesystemandroid/task/RegisterTask.java
@@ -1,13 +1,30 @@
 package com.yuebing.aicoursesystemandroid.task;
 
 import com.yuebing.aicoursesystemandroid.model.User;
+import com.yuebing.aicoursesystemandroid.service.LoginNetService;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class RegisterTask implements Runnable {
 
     private User user;
 
+    public RegisterTask(User user) {
+        this.user = user;
+    }
+
     @Override
     public void run() {
+        try {
+
+
+            Boolean isSuccess = LoginNetService.register(user);
+
+        } catch (IOException | JSONException e) {
+
+        }
 
     }
 }
--- app/src/main/java/com/yuebing/aicoursesystemandroid/ui/RegisterActivity.java
+++ app/src/main/java/com/yuebing/aicoursesystemandroid/ui/RegisterActivity.java
@@ -9,14 +9,27 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.yuebing.aicoursesystemandroid.R;
+import com.yuebing.aicoursesystemandroid.model.User;
 import com.yuebing.aicoursesystemandroid.task.RegisterTask;
+
+import top.androidman.SuperButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private TextView tv_info;
-    private Button bt_register;
-    private Button bt_teacher;
-    private Button bt_student;
+    private SuperButton bt_register;
+    private SuperButton bt_teacher;
+    private SuperButton bt_student;
+
+    private TextView tv_userId;
+    private TextView tv_password;
+    private TextView tv_username;
+
+    private int role;
+    private int userid;
+    private String username;
+    private String password;
+
 
 
     @Override
@@ -28,9 +41,9 @@ public class RegisterActivity extends AppCompatActivity {
         bt_register = findViewById(R.id.bt_registerConfirm);
         bt_student = findViewById(R.id.bt_student);
         bt_teacher = findViewById(R.id.bt_teacher);
-
-
-
+        tv_userId = findViewById(R.id.tv_registerUserid);
+        tv_password = findViewById(R.id.tv_registerPassword);
+        tv_username = findViewById(R.id.tv_registerUsername);
         TextPaint tp = tv_info.getPaint();
         tp.setFakeBoldText(true);
 
@@ -40,23 +53,46 @@ public class RegisterActivity extends AppCompatActivity {
         bt_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                userid = Integer.parseInt(tv_userId.getText().toString());
+                password = tv_password.getText().toString();
+                username = tv_username.getText().toString();
+                User user = new User();
+                user.setPassword(password);
+                user.setRole(role);
+                user.setUsername(username);
+                user.setUserid(userid);
+                user.setSex(0);
 
+                new Thread(new RegisterTask(user)).start();
             }
         });
 
+
+        /**
+         * 角色选择
+         */
         bt_teacher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                role = 1;
+                bt_teacher.setColorNormal(getResources().getColor(R.color.colorButtonPressed));
 
+                bt_student.setColorNormal(getResources().getColor(R.color.colorDisabledButton));
             }
         });
 
         bt_student.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                role = 0;
+
+                bt_student.setColorNormal(getResources().getColor(R.color.colorButtonPressed));
+
+                bt_teacher.setColorNormal(getResources().getColor(R.color.colorDisabledButton));
 
             }
         });
+
     }
 
 
